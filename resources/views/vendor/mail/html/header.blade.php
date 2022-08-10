597f87e3c7333a4c9c5be9b06a92336d5cf659f0
vicSystems <victorasuquob@gmail.com>
2022-08-10 16:50:16 +0100

    jade still

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 {{-- @if (trim($slot) === 'Laravel') --}}
-<img src="{{config('app.url')}}images/logo-dark.png" class="logo" alt="MetaSchools Logo">
+<img src="{{config('app.url')}}images/logo-dark.png" class="" alt="MetaSchools Logo">
 {{-- @else --}}
 {{-- {{ $slot }}
 @endif --}}
